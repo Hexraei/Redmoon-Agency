c90f03cd95804a0596080ec9e0d6a60d648b2808
Hexraei <vnavin7714@gmail.com>
2025-10-26 21:39:48 +0530

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import '../globals.css'
+import './globals.css'
 import { TRPCProvider } from '@/providers/trpc-provider'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
